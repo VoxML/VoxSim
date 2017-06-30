774d06e0beac948e8d00e736e786b8640a59516a
nkrishnaswamy <nkrishna@brandeis.edu>
2017-06-30 09:19:06 -0400

    Autobuilder tweaks - iOS

--- build_ios.sh
+++ build_ios.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd ../VoxSim-Mobile
-echo $(pwd)
 git submodule foreach git pull origin master
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -projectpath $(pwd) -executeMethod AutoBuilder.BuildIOS VoxSim -quit
 cd ../VoxSim
 cp deploy_script.sh ../VoxSim-Mobile/Build/ios/VoxSim
-cp -r Data/voxml ../VoxSim-Mobile/Build/ios/VoxSim/VoxML
+cp -r Data/voxml ../VoxSim-Mobile/Build/ios/VoxSim/VoxML/voxml
