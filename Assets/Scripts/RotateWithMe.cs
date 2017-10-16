09f5d593bf9db59f1dc3ef403315aedb84c15627
Isaac Wang <wangi@ufl.edu>
2017-10-16 15:17:29 -0400

    Add FixHandRotation script to prevent contortion

--- Assets/Scripts/RotateWithMe.cs
+++ Assets/Scripts/RotateWithMe.cs
@@ -18,6 +18,8 @@ public class RotateWithMe : MonoBehaviour {
 	private Vector3 startOrientation;
 	private Vector2 flatStart;
 
+	public bool debug;
+
 	// Use this for initialization
 	void Start () {
 		startPosition = transform.position;
@@ -34,27 +36,40 @@ public class RotateWithMe : MonoBehaviour {
 		Vector3 currentOrientation = (transform.position - sourcePosition).normalized;
 		Vector2 flatCurrent = ReduceDimensions (currentOrientation);
 
-		// Calculate arc from start to current
-		float angle = Vector2.Angle(flatStart, flatCurrent);
+		// Calculate angle from start to current
+		float angle = GetAngleBetween(flatStart, flatCurrent);
 
-		// Calculate desired angle
+		// Calculate desired rotation
 		Vector3 currentRotation = transform.rotation.eulerAngles;
 
 		switch (rotateAround) {
 		case Axis.X:
-			currentRotation.x = startEulerRotation.x - angle;
+			currentRotation.x = startEulerRotation.x + angle;
 			break;
 		case Axis.Y:
 		default:
-			currentRotation.y = startEulerRotation.y - angle;
+			currentRotation.y = startEulerRotation.y + angle;
 			break;
 		case Axis.Z:
-			currentRotation.z = startEulerRotation.z - angle;
+			currentRotation.z = startEulerRotation.z + angle;
 			break;
 		}
 
 		// Apply to transform
 		transform.rotation = Quaternion.Euler(currentRotation);
+	}
+
+	public static float GetAngleBetween(Vector2 from, Vector2 to) {
+		float angle = (Mathf.Atan2 (from.y, from.x) - Mathf.Atan2(to.y, to.x)) * Mathf.Rad2Deg;
+
+		// Constrain within range -180 to 180
+		if (angle > 180) {
+			angle -= 360;
+		} else if (angle < -180) {
+			angle += 360;
+		}
+
+		return angle;
 	}
 
 	Vector2 ReduceDimensions(Vector3 input) {
