8b629ecaed0e72a15fa4da9f9244c4e4921aa43b
Isaac Wang <wangi@ufl.edu>
2017-11-06 19:05:15 -0500

    Add gesture start/end events to AvatarGestureController

--- Assets/GestureAnimation/Scripts/AvatarGestureController.cs
+++ Assets/GestureAnimation/Scripts/AvatarGestureController.cs
@@ -9,6 +9,7 @@ public class AvatarGestureController : MonoBehaviour {
     public GameObject avatar;
 
     private Animator animator;
+    private AvatarGestureStateBehavior agsBehavior;
 
     private const string ANIM_RARM_GESTUREID = "RArm_GestureId";
     private const string ANIM_RARM_TRIGGER = "RArm_Trigger";
@@ -16,7 +17,41 @@ public class AvatarGestureController : MonoBehaviour {
     private const string ANIM_LARM_GESTUREID = "LArm_GestureId";
     private const string ANIM_LARM_TRIGGER = "LArm_Trigger";
 
-    // Use this for initialization
+    //
+    //  Properties
+    //
+
+    public AvatarGesture CurrentGesture { get; private set; }
+
+    public string CurrentGestureName { get { return CurrentGesture == null ? "" : CurrentGesture.Name; } }
+
+    public bool IsGesturing { get; private set; }
+
+    //
+    //  Events
+    //
+
+    /// <summary>
+    /// Event Handler for AvatarGestureController events.
+    /// </summary>
+    /// <param name="sender">Reference to the AvatarGestureController</param>
+    /// <param name="gesture">The current gesture</param>
+    public delegate void AvatarGestureEventHandler(object sender, AvatarGesture gesture);
+
+    /// <summary>
+    /// GestureStart is called when PerformGesture is called and the animation begins playing.
+    /// </summary>
+    public event AvatarGestureEventHandler GestureStart;
+
+    /// <summary>
+    /// GestureEnd is called after an animation is completed.
+    /// </summary>
+    public event AvatarGestureEventHandler GestureEnd;
+
+    //
+    //  Initialization
+    //
+    
     void Start () {
 		if (!avatar)
         {
@@ -24,12 +59,42 @@ public class AvatarGestureController : MonoBehaviour {
         }
 
         animator = avatar.GetComponent<Animator>();
+        agsBehavior = animator.GetBehaviour<AvatarGestureStateBehavior>(); // TODO: Multiple layers may have different state behaviors!
+
+        // Attach event handlers to state machine behavior (allows us to get animator events)
+        agsBehavior.AnimationStart += StateMachineBehavior_AnimationStart;
+        agsBehavior.AnimationEnd += StateMachineBehavior_AnimationEnd;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update () {
 		
 	}
+
+    //
+    //  Event Handlers
+    //
+
+    private void StateMachineBehavior_AnimationStart()
+    {
+        IsGesturing = true;
+        if (GestureStart != null)
+        {
+            GestureStart(this, CurrentGesture);
+        }
+    }
+    
+    private void StateMachineBehavior_AnimationEnd()
+    {
+        IsGesturing = false;
+        if (GestureEnd != null)
+        {
+            GestureEnd(this, CurrentGesture);
+        }
+    }
+
+    //
+    //  Methods
+    //
 
     /// <summary>
     /// Editor-compatible API. Makes the avatar perform the specified gesture.
@@ -71,7 +136,8 @@ public class AvatarGestureController : MonoBehaviour {
             return;
         }
 
-        // Get correct trigger names based on handedness
+        //////////////////////////////////////////////
+        // Get trigger names based on handedness
         string anim_gestureid_name;
         string anim_trigger_name;
 
@@ -80,21 +146,31 @@ public class AvatarGestureController : MonoBehaviour {
             anim_gestureid_name = ANIM_LARM_GESTUREID;
             anim_trigger_name = ANIM_LARM_TRIGGER;
         }
-        else
+        else //if (gesture.BodyPart == AvatarGesture.Body.RightArm) // TODO: Right now, just left or right
         {
             anim_gestureid_name = ANIM_RARM_GESTUREID;
             anim_trigger_name = ANIM_RARM_TRIGGER;
         }
 
+        //////////////////////////////////////////////
         // Trigger the gesture
         animator.SetInteger(anim_gestureid_name, gesture.Id);
         animator.SetTrigger(anim_trigger_name);
 
+        CurrentGesture = gesture;
+
+        //////////////////////////////////////////////
         // Callback upon completion
         if (callback != null)
         {
-            // TODO -- incorrect timing for callback
-            callback(gesture);
+            // Create a one-shot event handler that calls the callback function
+            AvatarGestureStateBehavior.StateMachineEventHandler eventHandler = null;
+            eventHandler = delegate ()
+            {
+                agsBehavior.AnimationEnd -= eventHandler; // Unsubscribe; one-shot
+                callback(gesture);
+            };
+            agsBehavior.AnimationEnd += eventHandler; // Subscribe
         }
     }
 }
--- Assets/GestureAnimation/Scripts/AvatarGestureTestUI.cs
+++ Assets/GestureAnimation/Scripts/AvatarGestureTestUI.cs
@@ -8,7 +8,11 @@ public class AvatarGestureTestUI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        // Example of subscribing to gesture event
+        gestureController.GestureStart += delegate (object sender, AvatarGesture ag)
+        {
+            Debug.Log("Gesture Start: " + ag.Name + " (IsGesturing=" + gestureController.IsGesturing + ")");
+        };
 	}
 	
 	// Update is called once per frame
@@ -18,6 +22,10 @@ public class AvatarGestureTestUI : MonoBehaviour {
 
     public void TriggerGestureAnimation(InputField textInput)
     {
-        gestureController.PerformGesture(textInput.text);
+        // Example of using callback along with PerformGesture
+        gestureController.PerformGesture(textInput.text, delegate(AvatarGesture ag)
+        {
+            Debug.Log("Gesture End: " + ag.Name + " (IsGesturing=" + gestureController.IsGesturing + ")");
+        });
     }
 }
