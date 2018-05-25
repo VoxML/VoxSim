f7ef126ec95316e90b6362fe9792d67db6da367a
nkrishnaswamy <nkrishna@brandeis.edu>
2018-05-25 11:17:21 -0400

    Added 'BlockUnavailable' state if block unknown or unaccessible

--- Assets/Scripts/Demos/JointGestureDemo.cs
+++ Assets/Scripts/Demos/JointGestureDemo.cs
@@ -2865,6 +2865,36 @@ public class JointGestureDemo : AgentInteraction {
 		interactionLogic.RewriteStack (new PDAStackOperation (PDAStackOperation.PDAStackOperationType.Rewrite,null));
 	}
 
+	public void BlockUnavailable(object[] content) {
+		if (interactionLogic.GraspedObj != null) {
+			if (interactionLogic.ActionOptions.Count > 0) {
+				if ((Regex.IsMatch (interactionLogic.ActionOptions [interactionLogic.ActionOptions.Count - 1], "lift")) ||
+					(Regex.IsMatch (interactionLogic.ActionOptions [interactionLogic.ActionOptions.Count - 1], "put")) ||
+					(Regex.IsMatch (interactionLogic.ActionOptions [interactionLogic.ActionOptions.Count - 1], "grab move"))) {
+					PromptEvent (string.Format ("put({0},{1})", 
+						interactionLogic.GraspedObj.name,
+						Helper.VectorToParsable (new Vector3 (interactionLogic.GraspedObj.transform.position.x,
+							Helper.GetObjectWorldSize (demoSurface).max.y,
+							interactionLogic.GraspedObj.transform.position.z))));
+				}
+				else {
+					PromptEvent (string.Format ("ungrasp({0})", interactionLogic.GraspedObj.name));
+				}
+			}
+			else {
+				PromptEvent (string.Format ("ungrasp({0})", interactionLogic.GraspedObj.name));
+			}
+		}
+		else {
+			LookForward ();
+			TurnForward ();
+		}
+			
+		RespondAndUpdate ("Sorry, I can't find a block like that that I can use.");
+
+		interactionLogic.RewriteStack (new PDAStackOperation (PDAStackOperation.PDAStackOperationType.Rewrite,null));
+	}
+
 	public void Confusion(object[] content) {
 		if (interactionLogic.GraspedObj != null) {
 			if (interactionLogic.ActionOptions.Count > 0) {
