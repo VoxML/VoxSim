634d14d8037716b7ab6bf38b378d33e4df7c5a7a
nkrishnaswamy <nkrishna@brandeis.edu>
2019-06-10 11:14:01 -0400

    Imported new Standard Assets package

--- Assets/Standard Assets/Utility/CameraRefocus.cs
+++ Assets/Standard Assets/Utility/CameraRefocus.cs
@@ -1,49 +1,58 @@
+using System;
 using UnityEngine;
 
-namespace UnityStandardAssets.Utility {
-	public class CameraRefocus {
-		public Camera Camera;
-		public Vector3 Lookatpoint;
-		public Transform Parent;
+namespace UnityStandardAssets.Utility
+{
+    public class CameraRefocus
+    {
+        public Camera Camera;
+        public Vector3 Lookatpoint;
+        public Transform Parent;
 
-		private Vector3 m_OrigCameraPos;
-		private bool m_Refocus;
-
-
-		public CameraRefocus(Camera camera, Transform parent, Vector3 origCameraPos) {
-			m_OrigCameraPos = origCameraPos;
-			Camera = camera;
-			Parent = parent;
-		}
+        private Vector3 m_OrigCameraPos;
+        private bool m_Refocus;
 
 
-		public void ChangeCamera(Camera camera) {
-			Camera = camera;
-		}
+        public CameraRefocus(Camera camera, Transform parent, Vector3 origCameraPos)
+        {
+            m_OrigCameraPos = origCameraPos;
+            Camera = camera;
+            Parent = parent;
+        }
 
 
-		public void ChangeParent(Transform parent) {
-			Parent = parent;
-		}
+        public void ChangeCamera(Camera camera)
+        {
+            Camera = camera;
+        }
 
 
-		public void GetFocusPoint() {
-			RaycastHit hitInfo;
-			if (Physics.Raycast(Parent.transform.position + m_OrigCameraPos, Parent.transform.forward, out hitInfo,
-				100f)) {
-				Lookatpoint = hitInfo.point;
-				m_Refocus = true;
-				return;
-			}
-
-			m_Refocus = false;
-		}
+        public void ChangeParent(Transform parent)
+        {
+            Parent = parent;
+        }
 
 
-		public void SetFocusPoint() {
-			if (m_Refocus) {
-				Camera.transform.LookAt(Lookatpoint);
-			}
-		}
-	}
+        public void GetFocusPoint()
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Parent.transform.position + m_OrigCameraPos, Parent.transform.forward, out hitInfo,
+                                100f))
+            {
+                Lookatpoint = hitInfo.point;
+                m_Refocus = true;
+                return;
+            }
+            m_Refocus = false;
+        }
+
+
+        public void SetFocusPoint()
+        {
+            if (m_Refocus)
+            {
+                Camera.transform.LookAt(Lookatpoint);
+            }
+        }
+    }
 }
