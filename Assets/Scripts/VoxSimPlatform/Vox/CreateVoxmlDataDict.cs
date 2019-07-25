298df0f8d67f2b2c7b2d5e8c9b951fbb83a2abbe
Jin Zhao <jinzhao@brandeis.edu>
2019-07-25 14:39:06 -0400

    Load all VoxML into memory at runtime

--- Assets/Scripts/VoxSimPlatform/Vox/CreateVoxmlDataDict.cs
+++ Assets/Scripts/VoxSimPlatform/Vox/CreateVoxmlDataDict.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -7,12 +8,17 @@ using UnityEngine;
 /// </summary>
 public class CreateVoxmlDataDict : MonoBehaviour
 {
-    public Dictionary<string, string> VoxmlDataDict; 
+    public Dictionary<string, string> VoxmlDataDict;
+    public Dictionary<string, VoxSimPlatform.Vox.VoxML> VoxmlObjectDict; 
 
     void Start()
     {
         VoxmlDataDict = new Dictionary<string, string>();
-        WalkDir("Data/voxml"); 
+        WalkDir("Data/voxml");
+
+        VoxmlObjectDict = new Dictionary<string, VoxSimPlatform.Vox.VoxML>();
+        VoxSimPlatform.Vox.VoxML.LoadedFromText += OnLoadedFromText; 
+
     }
 
     private void WalkDir(string sDir)
@@ -28,9 +34,28 @@ public class CreateVoxmlDataDict : MonoBehaviour
                 WalkDir(d);
             }
         }
-        catch (System.Exception excpt)
+        catch (Exception excpt)
         {
             Debug.Log(excpt.Message);
         }
+    }
+
+    public void OnLoadedFromText(object sender, VoxSimPlatform.Vox.VoxMLObjectEventArgs e)
+    {
+        CreateVoxmlObjectDict(e.Filename, e.VoxML); 
+    }
+
+    public void CreateVoxmlObjectDict(string filename, VoxSimPlatform.Vox.VoxML voxML)
+    {
+        if (!VoxmlObjectDict.ContainsKey(filename)) {
+            VoxmlObjectDict.Add(Path.GetFileName(filename), voxML);
+        }
+
+        string s = ""; 
+        foreach (KeyValuePair<string, VoxSimPlatform.Vox.VoxML> kvp in VoxmlObjectDict)
+        {
+            s += string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        }
+        Debug.Log("Now printing dictionary**********:" + s); 
     }
 }
